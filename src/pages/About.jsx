3805f26fefb56a152edffe405f3a0106b3166aee
GregoryVDev <arexo@hotmail.fr>
2025-08-05 16:43:41 +0200

    Moved the images folder in public folder and added about className with an image

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,11 @@ import { Header } from "../Components/Header";
 
 export const About = () => {
   return (
+    <div>
       <Header />
+      <div className="about">
+      <img src="../assets/images/moi.jpg" alt="Photo de moi" />
+      </div>
+    </div>
   );
 };
