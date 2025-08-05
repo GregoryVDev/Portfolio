23facd8fa6bdf971b1e46833bda827b6106ad6a4
GregoryVDev <arexo@hotmail.fr>
2025-08-05 13:58:32 +0200

    Added react-router-dom library and imported BrowserRouter, Routes and Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 
 export function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} /> 
+    </Routes>
+    </BrowserRouter>
+  );
 }
