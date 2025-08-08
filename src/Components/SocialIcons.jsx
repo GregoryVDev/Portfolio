7ba49d9fa272308ae038f33b7d9dcb02af822013
GregoryVDev <arexo@hotmail.fr>
2025-08-08 17:30:38 +0200

    Edited color for the h2 title and added link for the linkedin, github, mailto and for download the CV

--- src/Components/SocialIcons.jsx
+++ src/Components/SocialIcons.jsx
@@ -4,16 +4,16 @@ import { IoMailOutline } from "react-icons/io5";
 export const SocialIcons = () => {
   return (
     <div className="medias">
-      <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="media">
+      <a href="https://www.linkedin.com/in/gregoryvdev/" target="_blank" rel="noopener noreferrer" className="media">
         <FaLinkedin />
       </a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="media">
+      <a href="https://github.com/GregoryVDev" target="_blank" rel="noopener noreferrer" className="media">
         <FaGithub />
       </a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="media">
+      <a href="mailto:gregoryvdev@gmail.com" target="_blank" rel="noopener noreferrer" className="media">
         <IoMailOutline  />
       </a>
-      <a href="https://github.com" target="_blank" rel="noopener noreferrer" className="media">
+      <a href="../../public/images/gregory_yeramian_dwwm_alternance.pdf" download target="_blank" rel="noopener noreferrer" className="media">
         <FaFileDownload   />
       </a>
     </div>
