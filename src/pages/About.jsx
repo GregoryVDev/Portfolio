7aec1b8443beaa323e5b074af929aef701d9b5f0
GregoryVDev <arexo@hotmail.fr>
2025-10-06 18:32:40 +0200

    Added properties for the title in infos section and paragraph in infos section. Added content for the about and added const faqData for the questions answer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,65 @@
 import { Header } from "../Components/Header";
 
 export const About = () => {
+  // Liste des questions et de réponses
+  const faqData = [
+    {
+      question: "Qui suis-je ?",
+      answer:
+        "Je m'appelle Grégory, j'ai 29 ans et je suis un ancien second de cuisine. J'habite à côté de Libourne et je suis en reconversion professionnelle pour devenir Concépteur Développeur d'Application.",
+    },
+    {
+      question:
+        "Pourquoi j'ai choisi de quitter la cuisine pour m'orienter vers le développement web ?",
+      answer:
+        "Après 15 ans derrière les fourneaux, j'ai décidé de relever de nouveaux défis tout en restant dans la créativité. Le code me permet de construire, tester et améliorer mes projets. (Un peu comme une recette, mais sans les brûlures !)",
+    },
+    {
+      question: "Qu'est-ce que je préfère dans le développement web ?",
+      answer:
+        "Ce que j'adore c'est de voir un projet qui prend vie ligne par ligne, résoudre des problèmes, comprendre pourquoi et trouver une solution.",
+    },
+    {
+      question: "Quel est mon objectif professionnel ?",
+      answer:
+        "Mon objectif professionnel, c'est de trouver une alternance afin de poursuivre mes études en bachelor en tant que Concépteur Développeur d'Application.",
+    },
+  ];
+
+  // Ouvrir les questions ouvertes
+  const [activeIndex, setActiveIndex] = usestate(null);
+
+  const toggleFAQ = (index) => {
+    setActiveIndex(index === activeIndex ? null : index);
+  };
+
   return (
     <div>
       <Header />
       <section className="infos">
         <h1>À propos de moi</h1>
+        <p>
+          Titulaire d'un C.A.P Cuisine obtenu en 2013, j'ai exercé ce métier
+          pendant douze années, ce qui m'a permis de développer des compétences
+          solides en organisation, sens des responsabilités, créativité et
+          rapidité d'exécution.
+        </p>
+        <p>
+          En 2024, j'ai décidé de faire une reconversion dans le développement
+          web, un domaine qui me passionne et dans lequel je retrouve les mêmes
+          valeurs qu'en cuisine : organisation, rapidité, sens des
+          responsabilités et créativité.
+        </p>
+        <p></p>
+        <p>
+          Ce que j'aime dans ce métier, c'est la créativité. J'apprécie autant
+          travailler sur le front-end que le back-end. J'y trouve toujours de
+          nouveaux défis, et j'ai envie d'apprendre de nouvelles technologies.
+        </p>
+        <p>
+          Je sais qu'il me reste encore beaucoup à apprendre, mais je suis
+          motivé, ambitieux et toujours prêt à relever de nouveaux défis.
+        </p>
       </section>
     </div>
   );
