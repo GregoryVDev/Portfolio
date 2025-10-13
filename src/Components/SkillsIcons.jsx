9429cb95d88a67a7d3879c73f1f99fba22d8da0e
GregoryVDev <arexo@hotmail.fr>
2025-10-13 16:20:56 +0200

    Added properties for the className, added className for the group in front-end category

--- src/Components/SkillsIcons.jsx
+++ src/Components/SkillsIcons.jsx
@@ -9,29 +9,72 @@ const techCategories = [
     category: "FRONT-END",
     techs: [
       {
-        name: "HTML",
-        icon: <img src="../../public/images/html5.svg" alt="HTML" />,
+        name: "HTML & CSS",
+        icon: (
+          <div className="html-css-group">
+            <img src="../../public/images/html5.svg" alt="HTML" />
+            <img src="../../public/images/css3.svg" alt="CSS" />
+          </div>
+        ),
       },
       {
-        name: "CSS",
-        icon: <img src="../../public/images/css3.svg" alt="CSS" />,
+        name: "JavaScript & React",
+        icon: (
+          <div className="js-react-group">
+            <img src="../../public/images/javascript.svg" alt="JS" />
+            <img src="../../public/images/react.svg" alt="React" />
+          </div>
+        ),
       },
-      { name: "JavaScript", icon: <FaNodeJs /> },
-      { name: "React", icon: <SiReact /> },
+      {
+        name: "Bootstrap & SASS",
+        icon: (
+          <div className="boot-sass-group">
+            <img src="../../public/images/bootstrap.svg" alt="Bootstrap" />
+            <img src="../../public/images/sass.svg" alt="SASS" />
+          </div>
+        ),
+      },
     ],
   },
   {
     category: "BACK-END",
     techs: [
-      { name: "PHP", icon: <FaPhp /> },
-      { name: "MySQL", icon: <SiMysql /> },
-      { name: "Node.js", icon: <FaNodeJs /> },
+      {
+        name: "PHP",
+        icon: <img src="../../public/images/php.svg" alt="PHP" />,
+      },
+      {
+        name: "MySQL",
+        icon: <img src="../../public/images/mysql.svg" alt="MySQL" />,
+      },
+      {
+        name: "Docker",
+        icon: <img src="../../public/images/docker.svg" alt="Docker" />,
+      },
     ],
   },
   {
     category: "UX & UI",
     techs: [
-      { name: "Figma", icon: <img src="/assets/figma.svg" alt="Figma" /> },
+      {
+        name: "Figma",
+        icon: <img src="../../public/images/figma.svg" alt="Figma" />,
+      },
+    ],
+  },
+  {
+    category: "Outils",
+    techs: [
+      {
+        name: "Github",
+        icon: (
+          <img
+            src="../../public/images/github_logo_icon_229278.png"
+            alt="Github"
+          />
+        ),
+      },
     ],
   },
 ];
