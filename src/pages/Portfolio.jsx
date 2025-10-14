240d89536e416359647cc43787b0161e11905068
GregoryVDev <arexo@hotmail.fr>
2025-10-14 17:05:15 +0200

    Added images for the world travel project with a new container-portfolio. Added container-project className

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -6,12 +6,27 @@ export const Portfolio = () => {
     <div>
       <Header />
       <div className="container-portfolio">
-        <img src="../../public/images/jadoopc.png" alt="Jadoo" />
-        <img
-          src="../../public/images/jadoomobile.png"
-          alt="Jadoo"
-          className="mobile"
-        />
+        <div className="container-project">
+          <img src="../../public/images/jadoopc.png" alt="Jadoo" />
+          <img
+            src="../../public/images/jadoomobile.png"
+            alt="Jadoo"
+            className="mobile"
+          />
+        </div>
+        <div className="container-portfolio">
+          <div className="container-project">
+            <img
+              src="../../public/images/worldtravelpc.png"
+              alt="World Travel"
+            />
+            <img
+              src="../../public/images/worldtravelmobile.png"
+              alt="World Travel"
+              className="mobile"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
