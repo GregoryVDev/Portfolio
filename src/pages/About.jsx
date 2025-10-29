72e31eb8aaa2e0e34a77864349de0bfae227de9e
GregoryVDev <arexo@hotmail.fr>
2025-10-29 14:19:20 +0100

    Added media queries for desktop and added container-informations and container-faq in About component

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -44,60 +44,64 @@ export const About = () => {
   return (
     <div>
       <Header />
-      <section className="infos">
-        <h1>À propos de moi</h1>
-        <p>
-          Titulaire d'un C.A.P Cuisine obtenu en 2013, j'ai exercé ce métier
-          pendant douze années, ce qui m'a permis de développer des compétences
-          solides en organisation, sens des responsabilités, créativité et
-          rapidité d'exécution.
-        </p>
-        <p>
-          En 2024, j'ai décidé de faire une reconversion dans le développement
-          web, un domaine qui me passionne et dans lequel je retrouve les mêmes
-          valeurs qu'en cuisine : organisation, rapidité, sens des
-          responsabilités et créativité.
-        </p>
-        <p>
-          Ce que j'aime dans ce métier, c'est la créativité. J'apprécie autant
-          travailler sur le front-end que le back-end. J'y trouve toujours de
-          nouveaux défis, et j'ai envie d'apprendre de nouvelles technologies.
-        </p>
-        <p>
-          Je sais qu'il me reste encore beaucoup à apprendre, mais je suis
-          motivé, ambitieux et toujours prêt à relever de nouveaux défis.
-        </p>
-      </section>
-      <section className="faq">
-        <h2>Foire aux questions</h2>
-        <div className="faq-container">
-          {faqData.map((item, index) => (
-            <div key={index} className="faq-item">
-              <button
-                className={`faq-question ${
-                  openIndexes.includes(index) ? "active" : ""
-                }`}
-                onClick={() => toggleFAQ(index)}
-              >
-                {item.question}
-                <span className="faq-icon">
-                  {openIndexes.includes(index) ? "-" : "+"}
-                </span>
-              </button>
-              <div
-                className={`faq-answer ${
-                  openIndexes.includes(index) ? "show" : ""
-                }`}
-              >
-                <p>{item.answer}</p>
-              </div>
+      <div className="container-informations">
+        <section className="infos">
+          <h1>À propos de moi</h1>
+          <p>
+            Titulaire d'un C.A.P Cuisine obtenu en 2013, j'ai exercé ce métier
+            pendant douze années, ce qui m'a permis de développer des
+            compétences solides en organisation, sens des responsabilités,
+            créativité et rapidité d'exécution.
+          </p>
+          <p>
+            En 2024, j'ai décidé de faire une reconversion dans le développement
+            web, un domaine qui me passionne et dans lequel je retrouve les
+            mêmes valeurs qu'en cuisine : organisation, rapidité, sens des
+            responsabilités et créativité.
+          </p>
+          <p>
+            Ce que j'aime dans ce métier, c'est la créativité. J'apprécie autant
+            travailler sur le front-end que le back-end. J'y trouve toujours de
+            nouveaux défis, et j'ai envie d'apprendre de nouvelles technologies.
+          </p>
+          <p>
+            Je sais qu'il me reste encore beaucoup à apprendre, mais je suis
+            motivé, ambitieux et toujours prêt à relever de nouveaux défis.
+          </p>
+        </section>
+        <div className="container-faq">
+          <section className="faq">
+            <h2>Foire aux questions</h2>
+            <div className="faq-container">
+              {faqData.map((item, index) => (
+                <div key={index} className="faq-item">
+                  <button
+                    className={`faq-question ${
+                      openIndexes.includes(index) ? "active" : ""
+                    }`}
+                    onClick={() => toggleFAQ(index)}
+                  >
+                    {item.question}
+                    <span className="faq-icon">
+                      {openIndexes.includes(index) ? "-" : "+"}
+                    </span>
+                  </button>
+                  <div
+                    className={`faq-answer ${
+                      openIndexes.includes(index) ? "show" : ""
+                    }`}
+                  >
+                    <p>{item.answer}</p>
+                  </div>
+                </div>
+              ))}
             </div>
-          ))}
+          </section>
+          <section className="skills">
+            <SkillsIcons />
+          </section>
         </div>
-      </section>
-      <section className="skills">
-        <SkillsIcons />
-      </section>
+      </div>
     </div>
   );
 };
