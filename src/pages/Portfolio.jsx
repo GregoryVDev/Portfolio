c1674b1bd9f4ed17da9bab56dccd746e3946dec5
GregoryVDev <arexo@hotmail.fr>
2025-10-30 21:35:37 +0100

    Added display grid for the container-portfolio.  Added width 100% and display flex for the container-project className and edited the right position for the mobile img. Removed the container-portfolio className for each projects

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -16,99 +16,87 @@ export const Portfolio = () => {
               className="mobile"
             />
             <div className="container-buttons">
-              <a href="https://gregoryvdev.com/jadoo/" target="_blanket">
+              <a href="https://gregoryvdev.com/jadoo/" target="_blank">
                 <CgWebsite size="25px" />
                 Démo
               </a>
               <a
                 href="https://github.com/GregoryVDev/acs-jadoo"
-                target="_blanket"
+                target="_blank"
               >
                 <FaGithub size="25px" /> Source Code
               </a>
             </div>
           </article>
-          <div className="container-portfolio">
-            <article className="container-project">
-              <img
-                src="../../public/images/worldtravelpc.png"
-                alt="World Travel"
-              />
-              <img
-                src="../../public/images/worldtravelmobile.png"
-                alt="World Travel"
-                className="mobile"
-              />
-              <div className="container-buttons">
-                <a
-                  href="https://gregoryvdev.com/worldtravel/"
-                  target="_blanket"
-                >
-                  <CgWebsite size="25px" />
-                  Démo
-                </a>
-                <a
-                  href="https://github.com/GregoryVDev/WorldTravel"
-                  target="_blanket"
-                >
-                  <FaGithub size="25px" /> Source Code
-                </a>
-              </div>
-            </article>
-          </div>
-          <div className="container-portfolio">
-            <article className="container-project">
-              <img src="../../public/images/playverse.png" alt="PlayVerse" />
-              <img
-                src="../../public/images/playversemobile.png"
-                alt="PlayVerse"
-                className="mobile"
-              />
-              <div className="container-buttons">
-                <a
-                  href="https://gregoryvdev.com/playverse/index.php"
-                  target="_blanket"
-                >
-                  <CgWebsite size="25px" />
-                  Démo
-                </a>
-                <a
-                  href="https://github.com/GregoryVDev/PlayVerse"
-                  target="_blanket"
-                >
-                  <FaGithub size="25px" /> Source Code
-                </a>
-              </div>
-            </article>
-          </div>
-          <div className="container-portfolio">
-            <article className="container-project">
-              <img
-                src="../../public/images/countriesflags.png"
-                alt="Countries Flags"
-              />
-              <img
-                src="../../public/images/countriesflagsmobile.png"
-                alt="Countries Flags"
-                className="mobile"
-              />
-              <div className="container-buttons">
-                <a
-                  href="https://gregoryvdev.com/countriesflags/"
-                  target="_blanket"
-                >
-                  <CgWebsite size="25px" />
-                  Démo
-                </a>
-                <a
-                  href="https://github.com/GregoryVDev/CountryFlag"
-                  target="_blanket"
-                >
-                  <FaGithub size="25px" /> Source Code
-                </a>
-              </div>
-            </article>
-          </div>
+          <article className="container-project">
+            <img
+              src="../../public/images/worldtravelpc.png"
+              alt="World Travel"
+            />
+            <img
+              src="../../public/images/worldtravelmobile.png"
+              alt="World Travel"
+              className="mobile"
+            />
+            <div className="container-buttons">
+              <a href="https://gregoryvdev.com/worldtravel/" target="_blank">
+                <CgWebsite size="25px" />
+                Démo
+              </a>
+              <a
+                href="https://github.com/GregoryVDev/WorldTravel"
+                target="_blank"
+              >
+                <FaGithub size="25px" /> Source Code
+              </a>
+            </div>
+          </article>
+          <article className="container-project">
+            <img src="../../public/images/playverse.png" alt="PlayVerse" />
+            <img
+              src="../../public/images/playversemobile.png"
+              alt="PlayVerse"
+              className="mobile"
+            />
+            <div className="container-buttons">
+              <a
+                href="https://gregoryvdev.com/playverse/index.php"
+                target="_blank"
+              >
+                <CgWebsite size="25px" />
+                Démo
+              </a>
+              <a
+                href="https://github.com/GregoryVDev/PlayVerse"
+                target="_blank"
+              >
+                <FaGithub size="25px" /> Source Code
+              </a>
+            </div>
+          </article>
+          <article className="container-project">
+            <img
+              src="../../public/images/countriesflags.png"
+              alt="Countries Flags"
+            />
+            <img
+              src="../../public/images/countriesflagsmobile.png"
+              alt="Countries Flags"
+              className="mobile"
+            />
+            <div className="container-buttons">
+              <a href="https://gregoryvdev.com/countriesflags/" target="_blank">
+                <CgWebsite size="25px" />
+                Démo
+              </a>
+              <a
+                href="https://github.com/GregoryVDev/CountryFlag"
+                target="_blank"
+              >
+                <FaGithub size="25px" /> Source Code
+              </a>
+            </div>
+          </article>
         </div>
       </section>
     </div>
