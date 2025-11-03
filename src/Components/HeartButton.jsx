a17463f09a79eb343825fe8858675980774a7797
GregoryVDev <arexo@hotmail.fr>
2025-11-03 16:06:42 +0100

    Added red color and make the heart bounce when clicked and

--- src/Components/HeartButton.jsx
+++ src/Components/HeartButton.jsx
@@ -19,6 +19,22 @@ export default function HeartButton() {
       transition={{ type: "spring", stiffness: 300 }}
       className="flex items-center justify-center cursor-pointer select-none"
       style={{ width: "60px", height: "60px" }}
-    ></motion.div>
+    >
+      <motion.svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="60"
+        height="60"
+        // Il devient rouge si il est cliqué sinon il ne change pas
+        fill={liked ? "red" : "none"}
+        stroke="red"
+        strokeWidth="2"
+        // Petit rebond quand le coeur devient rouge
+        animate={{ scale: liked ? [1, 1.4, 1] : 1 }}
+        transition={{ duration: 0.3 }}
+      >
+        <path d="M12 21s-7-4.35-10-9a6 6 0 0 1 9-7c1 1 1.8 2 1.8 2s0.8-1 1.8-2a6 6 0 0 1 9 7c-3 4.65-10 9-10 9z" />
+      </motion.svg>
+    </motion.div>
   );
 }
